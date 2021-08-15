f2b2eb660257b276d1e3c26550849e5648ac44c3
Danilo Marques <danilomarques.cristo@gmail.com>
2021-08-15 15:27:23 -0300

    Adding tests for when should return 400.

--- test/user_test.go
+++ test/user_test.go
@@ -1,10 +1,11 @@
 package test
 
 import (
-	//"log"
+	"encoding/json"
 	"net/http"
 	"strings"
 	"testing"
+	"github.com/danilomarques1/findmypetapi/dto"
 )
 
 func TestCreateUser(t *testing.T) {
@@ -13,4 +14,25 @@ func TestCreateUser(t *testing.T) {
 	assertNil(t, err)
 	response := executeRequest(request)
 	assertEqual(t, http.StatusCreated, response.Code)
+
+	var createResponse dto.CreateUserResponseDto
+	err = json.NewDecoder(response.Body).Decode(&createResponse)
+	assertNil(t, err)
+	assertEqual(t, "Fitz", createResponse.User.Name)
+}
+
+func TestErrorCreateUser(t *testing.T) {
+	// missing name
+	body := `{"email": "fitz@gmail.com", "password": "123456", "confirm_password": "123456"}`
+	request, err := http.NewRequest(http.MethodPost, "/user", strings.NewReader(body))
+	assertNil(t, err)
+	response := executeRequest(request)
+	assertEqual(t, http.StatusBadRequest, response.Code)
+
+	// different passwords
+	body = `{"name": "Fitz", "email": "fitz@gmail.com", "password": "123456", "confirm_password": "123456890"}`
+	request, err = http.NewRequest(http.MethodPost, "/user", strings.NewReader(body))
+	assertNil(t, err)
+	response = executeRequest(request)
+	assertEqual(t, http.StatusBadRequest, response.Code)
 }
