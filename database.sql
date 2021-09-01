797a048adc420ab71cd3377597ca53bd380eda9e
Danilo Marques <danilomarques.cristo@gmail.com>
2021-08-31 21:46:29 -0300

    Adding post/comment tables on database.sql. adding enviroment variables examples.

--- database.sql
+++ database.sql
@@ -5,16 +5,30 @@ CREATE TABLE IF NOT EXISTS UserPet(
     password_hash VARCHAR(100) NOT NULL
 );
 
+DO $$
+BEGIN
+    IF NOT EXISTS (select * from pg_type where pg_type.typname = 'petstatus') THEN
+        CREATE TYPE PetStatus AS ENUM('missing', 'found');
+    END IF ;
+END$$;
+
 CREATE TABLE IF NOT EXISTS Post(
     id UUID NOT NULL PRIMARY KEY,
+    author_id UUID Not null,
     title VARCHAR(120) NOT NULL, 
-    description TEXT
+    description TEXT,
+    image_url VARCHAR(100),
+    status PetStatus,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT post_user_fk FOREIGN KEY(author_id) REFERENCES UserPet(id)
 );
 
-CREATE TABLE IF NOT EXISTS UserPost(
-    id uuid NOT NULL primary key,
+CREATE TABLE IF NOT EXISTS Comment(
+    id UUID NOT NULL PRIMARY KEY,
+    comment_text TEXT NOT NULL,
     user_id UUID NOT NULL,
     post_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
     CONSTRAINT user_fk FOREIGN KEY(user_id) REFERENCES UserPet(id),
     CONSTRAINT post_fk FOREIGN KEY(post_id) REFERENCES Post(id)
 );
