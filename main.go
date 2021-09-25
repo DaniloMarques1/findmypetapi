6810cff30f2f274ac4b1ca6e96823c4422757cac
Danilo Marques <danilomarques20@hotmail.com>
2021-09-25 13:48:49 -0300

    Workaroung to create the google credential file inside heroku based on env variable.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+
 	"github.com/danilomarques1/findmypetapi/app"
 	"github.com/joho/godotenv"
 )
@@ -14,6 +15,16 @@ func main() {
 	}
 	dbstring := fmt.Sprintf("host=%v dbname=%v password=%v user=%v",
 		os.Getenv("DB_HOST"), os.Getenv("DB_NAME"), os.Getenv("DB_PWD"), os.Getenv("DB_USER"))
+
+	credentials := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_CONTENT")
+	file, err := os.Create(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+	if err != nil {
+		log.Fatalf("Error getting google credentials... %v\n", err)
+	}
+
+	if _, err := file.WriteString(credentials); err != nil {
+		log.Fatalf("Error writing google credentials... %v\n", err)
+	}
 
 	var a app.App
 	a.Init("database.sql", dbstring)
